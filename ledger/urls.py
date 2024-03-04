42a526fd494c1f786c5a741f62755edfa39d8bfb
Raffy Taffeey <taffee.codes@gmail.com>
2024-03-04 22:41:45 +0800

    Certain changes for lab 2 were made

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import recipes_list
-from .views import recipe_1
-from .views import recipe_2
-urlpatterns = [
-path('recipes/list', recipes_list, name='recipes'),
-path('recipe/1/', recipe_1, name='recipe'),
-path('recipe/2/', recipe_2, name='recipe')
 
+from .views import RecipeListView, RecipeDetailView
+
+
+urlpatterns = [
+    path('recipes/list', RecipeListView.as_view(), name = 'list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name = 'recipe-detail')
 ]
-# This might be needed, depending on your Django version
+
 app_name = "ledger"
